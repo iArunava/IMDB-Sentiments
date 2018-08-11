293d4bd42704df3b40e18f00cd07ecac6bb598f9
Arunava <learningdroidarunava@gmail.com>
2018-08-11 15:57:10 +0530

    Completed appgit add .git add . App now workinggit add . Yaygit add .! Predicting sentiments like a reviewer! And better :P

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from tkinter import *
 from predict import *
 
-tfidf_vect, selector = get_ready()
+tfidf_vect, selector, model = get_ready()
+threshold = .6
 
 root = Tk()
-root.geometry("460x460")
+root.geometry("660x460")
 root.title('Sentiment Analysis using IMDB Sentiments')
 
 l1 = Label(root, text='Write your Review here:')
@@ -15,8 +16,11 @@ text.pack()
 
 def predict_btn_clicked():
     ttX = text.get('1.0', 'end');
-    prediction = predict(ttX, tfidf_vect, selector)
-    senti.set('sdfsd')
+    prediction = predict(ttX, tfidf_vect, selector, model)
+    sentiment = 'Positive' if prediction[0][0] > threshold else 'Negetive'
+    senti.set(str(sentiment) +
+              ' || Probability of the review being a positive sentiment is: ' +
+              str(prediction[0][0]))
 
 Button(root, text='Predict Sentiment', command=predict_btn_clicked).pack()
 
--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from keras.models import load_model
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -11,11 +12,13 @@ from sklearn.feature_selection import f_classif
 from sklearn.externals import joblib
 
 def get_ready():
-    tfidf_vect = joblib.load('./../assets/ti_vec.pkl')
-    selector = joblib.load('./../assets/selector.pkl')
-    return tfidf_vect, selector
+    tfidf_vect = joblib.load('./assets/ti_vec.pkl')
+    selector = joblib.load('./assets/selector.pkl')
+    model = load_model('./assets/IMDB_mlp_model_0.90.h5')
+    return tfidf_vect, selector, model
 
-def predict(ttX, tfidf_vect, selector):
-    x_val = tfidf_vect.transform(ttX)
+def predict(ttX, tfidf_vect, selector, model):
+    x_val = tfidf_vect.transform([ttX])
     x_val = selector.transform(x_val).astype('float32')
     # Predict model and return prediction
+    return model.predict(x_val)
